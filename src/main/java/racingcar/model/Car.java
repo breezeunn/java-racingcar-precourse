8ca4a62868c6660685c2faa1fadd498961651b76
breezeunn <je7689@gmail.com>
2022-04-25 00:09:28 +0900

    feat(Car): 자동차 정보 추가

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,68 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.platform.commons.util.StringUtils;
+
+public class Car {
+
+    private final String errorMessagePrefix = "[ERROR]";
+
+    private final String name;
+
+    private int position;
+
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+        this.position = 0;
+    }
+
+    private void validateName(String name) {
+        if (StringUtils.isBlank(name)) {
+            throw new IllegalArgumentException(errorMessagePrefix + " " + "이름이 유효하지 않습니다.");
+        }
+
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(errorMessagePrefix + " " + "이름은 최대 5글자까지 입력할 수 있습니다.");
+        }
+    }
+
+    public void doSubGame() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        doSubGame(randomNumber);
+    }
+
+    public void doSubGame(int randomNumber) {
+        validateRandomNumber(randomNumber);
+        if (randomNumber >= 4) {
+            forward();
+        }
+    }
+
+    public void validateRandomNumber(int randomNumber) {
+        if (randomNumber < 0 || randomNumber > 9) {
+            throw new IllegalArgumentException(errorMessagePrefix + " " + "랜덤 숫자 범위가 유효하지 않습니다.");
+        }
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void forward() {
+        this.position++;
+    }
+
+    public String getCurrentStatus() {
+        StringBuilder status = new StringBuilder(name);
+        status.append(" : ");
+        for (int i = 1; i <= getPosition(); i++) {
+            status.append("-");
+        }
+        return status.toString();
+    }
+}
---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,40 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CarTest {
+
+    @Test
+    void 자동차_이름이_공백인_경우() {
+        assertThrows(IllegalArgumentException.class, () -> new Car(null));
+    }
+
+    @Test
+    void 자동차_이름이_5글자를_초과하는_경우() {
+        assertThrows(IllegalArgumentException.class, () -> new Car("테스트자동차"));
+    }
+
+    @Test
+    void 자동차_전진() {
+        Car car = new Car("hello");
+        car.doSubGame(5);
+        assertEquals(car.getPosition(), 1);
+    }
+
+    @Test
+    void 자동차_멈춤() {
+        Car car = new Car("hello");
+        car.doSubGame(3);
+        assertEquals(car.getPosition(), 0);
+    }
+
+    @Test
+    void 자동차_전진후_상태() {
+        Car car = new Car("hello");
+        car.doSubGame(5);
+        assertEquals(car.getCurrentStatus(), "hello : -");
+    }
+}
